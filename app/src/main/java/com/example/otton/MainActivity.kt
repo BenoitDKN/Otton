3e8d7098ec7741138f59c69021caf28bd141d37f
BenoitDEKEYN <benoitdkn@gmail.com>
2023-10-16 21:48:53 +0200

    at

--- app/src/main/java/com/example/otton/MainActivity.kt
+++ app/src/main/java/com/example/otton/MainActivity.kt
@@ -4,16 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val connect = findViewById<Button>(R.id.connect)
-        connect.setOnClickListener(View.OnClickListener {
-            println("hello word")
-        })
+        connect.setOnClickListener {
+            Toast.makeText(this,"aaaa", Toast.LENGTH_LONG).show()
+        }
     }
 }
-ggggg
