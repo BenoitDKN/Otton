d04802580920fd32aa2d964118e1f3221c079ccf
BenoitDEKEYN <benoitdkn@gmail.com>
2023-10-17 10:34:56 +0200

    at

--- app/src/main/java/com/example/otton/MainActivity.kt
+++ app/src/main/java/com/example/otton/MainActivity.kt
@@ -2,17 +2,31 @@ package com.example.otton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.Toast
+import android.content.Context
+import android.media.AudioManager
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var audioManager: AudioManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val connect = findViewById<Button>(R.id.connect)
-        connect.setOnClickListener {
-            Toast.makeText(this,"aaaa", Toast.LENGTH_LONG).show()
+
+        //le code que j'ai ajouté :
+        audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        val buttonSetVolume = findViewById<Button>(R.id.connect)
+        buttonSetVolume.setOnClickListener {
+            setNotificationVolumeToPercentage()
         }
+
+
+    }
+    //la fonction que j'utilise que j'ai ajouté :
+    private fun setNotificationVolumeToPercentage() {
+        val percentage = 50
+        val maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_NOTIFICATION)
+        val desiredVolume = (maxVolume*percentage) / 100
+        audioManager.setStreamVolume(AudioManager.STREAM_NOTIFICATION, desiredVolume, 0)
     }
 }
+
